b01727ed5ac3e39f9c2e5c501dcfefefb2c2460f
Петр Борисов <i.love.angem@gmail.com>
2024-12-12 17:48:31 +0100

    chore: try change dos settings

--- scripts/index.js
+++ scripts/index.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const dosInstance = Dos(document.getElementById("dos"), {
     url: "https://doom-statiac.s3.eu-central-1.amazonaws.com/doom_jsbox_orig_s3.jsdos",
     background: "assets/bg/bg2.png",
+    autolock: true,
+    fullscreen: true
   });
 });
